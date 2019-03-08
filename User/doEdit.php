328b15e34f1df5b5f0223e5bd8bab08606c7fe9d
MarcoSamir <48352620+MarcoSamir@users.noreply.github.com>
2019-03-08 03:16:04 -0800

    Update doEdit.php

--- User/doEdit.php
+++ User/doEdit.php
@@ -1,10 +1,14 @@
 <?php
-include_once 'User.php';
+include_once 'Classes.php';
+session_start();
+$Object=new Users();
+$Object->Name = $_POST["Name"];
+$Object->Email = $_POST["Email"];
+$Object->Password = $_POST["Password"];
+$ID= $_SESSION["Id"];
+$Object->ID=$ID;
+Users::edit($Object);
 
-
-$ID=$_POST['ID'];
-
-User::edit($ID);
 
 
 
