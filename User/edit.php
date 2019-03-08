01bcd14a5ea115f94603dfe3489178f336a6bd85
MarcoSamir <48352620+MarcoSamir@users.noreply.github.com>
2019-03-08 03:11:11 -0800

    Update edit.php

--- User/edit.php
+++ User/edit.php
@@ -1,5 +1,8 @@
 
 
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,15 +11,58 @@
 <body>
 
 <?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "lab";
+session_start();
+// create connection
+$conn = new mysqli ($hostname, $username, $password, $dbname);
+  include_once 'Classes.php';
+  
+
+if (!isset($_SESSION)){
+    $ID;
+	$_SESSION["Id"];
+	$ID=$_SESSION["Id"];
+}
+
+//$obj=new Users($ID);
 
 
-echo "<form method='POST' action='".edit($ID)."'>
+//Users::Display($ID);
 
-<textarea  name='Name' value='>".$Name."</textarea>
-<textarea  name='Email' value='>".$Email."</textarea>
-<textarea  name='Password' value='>".$Password."</textarea>
-<button type='submit' name='signupbtn'>Edit</button>
-</form>";
+
+
+ $ID=$_SESSION["Id"];
+			   $obj=new Users($ID);
+			  $obj->Name=$_SESSION["Name"];
+			   $obj->Email=$_SESSION["Email"];
+			    $obj->Password=$_SESSION["Password"];
+
+echo "<form action='doEdit.php' method='post'>
+
+              <label> Name: <label>
+
+              <input type='text' value='".$_SESSION["Name"]."' name='Name'><br>
+
+             
+              <label> Email: <label>
+
+              <input type='text' value='".$_SESSION["Email"]."' name='Email'><br>
+
+              <label> Password: <label>
+
+              <input type='text' value='".$_SESSION["Password"]."' name='Password'><br>
+
+              
+    
+
+              <input type='submit' value='submit' name='submit'><br>
+
+              </form>";
+			 
+				Users::edit($obj);
 ?>
 
 </body>
